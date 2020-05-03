14dd8905fa377ace37c45a4a8dd552c49804f80b
filserm <55490122+filserm@users.noreply.github.com>
2020-05-03 16:56:49 +0200

    Add files via upload

---
+++ covid.py
@@ -0,0 +1,174 @@
+import json
+from datetime import datetime as dt
+from pprint import pprint
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as plticker
+import numpy as np
+from pylab import figure, text, scatter, show
+import re
+import subprocess
+from requests.auth import HTTPBasicAuth
+import requests
+import os
+
+url = 'https://api.covid19api.com/dayone/country/germany'
+url_IN = 'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=1%3D1&outFields=cases,county,last_update&returnGeometry=false&outSR=4326&f=json'
+url_by = 'https://covid19-germany.appspot.com/timeseries/DE-BY/cases'
+
+#url_new_cases = 'https://api.covid19api.com/dayone/country/germany/status/confirmed/live'
+#url_all_data = 'https://api.coronatab.app/places/closest'
+
+days = 30
+rolling_window_avg = 20
+
+def retrieve_covid_data():
+    data = pd.DataFrame([])
+    #data = pd.read_json(url)
+    #data_by = pd.read_json(url_by)
+    #data_IN = pd.read_json(url_IN)
+    '''
+    retrieve_messages = requests.get(url_IN)
+    data_IN = retrieve_messages.json()
+
+    loaded_json = json.dumps(data_IN)
+    for x in data_IN:
+           if re.search('Ingolstadt', str(data_IN[x])):
+               data_all = data_IN[x]
+
+    for y, z in enumerate(data_all):
+        print (y, z)
+
+    pprint (data_all[222])
+    exit()
+    '''
+
+    retrieve_messages = requests.get(url_by)
+    data_by = retrieve_messages.json()
+
+    data_all_by = []
+    for x in data_by:
+        #pprint (data_by[x])
+        data_all_by.extend([data_by[x] for x in data_by])
+
+    cols = ['datum', 'ConfirmedCases']
+    lst = []
+    for item in data_all_by[0]:
+        for k,v in item.items():
+            lst.append([k, v])
+    data_by = pd.DataFrame(lst, columns=cols)
+    data_by['datum'] = [ts[0:10] for ts in data_by['datum']]
+    data_by['DeltaConfirmed'] = data_by['ConfirmedCases'].diff()
+    data_by['mean_last20days'] = data_by.DeltaConfirmed.rolling(window=rolling_window_avg,min_periods=0).mean()
+    data_by.to_csv (r'export_data_by.csv', index = False, header=True)
+    pprint (data_by)
+
+    #pprint (list(data.columns.values))
+
+    if data.empty:
+        print ("API Germany is not responding ...")
+        #data = ['no data']
+    else:
+        #letzte Zeile dropen
+        data.drop(data.tail(1).index,inplace=True)
+        data['datum'] = [ts[0:10] for ts in data['Date']]
+        data['DeltaConfirmed'] = data['Confirmed'].diff()
+        data['mean_last20days'] = data.DeltaConfirmed.rolling(window=rolling_window_avg,min_periods=0).mean()
+        data['DeltaPercentage'] = data.DeltaConfirmed.pct_change() * 100
+        data.to_csv (r'export_data.csv', index = False, header=True)
+        pprint (data.tail(days))
+
+
+    return data, data_by
+
+
+def plot_data(data, data_by):
+    f = plt.figure(figsize = (20, 8))
+    plt.style.use('dark_background')
+    #move_figure(f, 540, 0)
+
+    ''' letzten x Tage ausgeben '''
+    latest_x_days = data.tail(days)
+    latest_x_days_by = data_by.tail(days)
+    #print (latest_x_days_by)
+
+
+    ax = plt.subplot(111)
+
+    text(0.5, 0.95,'Covid19 - confirmed cases Germany',
+         horizontalalignment='center',
+         verticalalignment='center',
+         fontsize=16,
+         transform = ax.transAxes)
+
+    text(0.5, 0.9,'data source: https://covid19api.com/\nCenter for Systems Science and Engineering (CSSE) at Johns Hopkins University',
+         horizontalalignment='center',
+         verticalalignment='center',
+         fontsize=12,
+         transform = ax.transAxes)
+
+    #plt.title('Covid19 - confirmed cases Germany')
+    #plt.bar(latest_x_days['datum'], latest_x_days['DeltaConfirmed'], align='center', label='confirmed cases')
+    plt.bar(latest_x_days_by['datum'], latest_x_days_by['DeltaConfirmed'], align='center', label='confirmed cases')
+    plt.xlabel("Datum",  fontsize = 8)
+    ax.tick_params(axis="x", labelsize=8, colors='white')
+    ax.tick_params(axis="y", labelsize=8, colors='white')
+
+    #ax.set_xticklabels(latest_x_days['datum'], color='black')
+    # Make some labels.
+    ''' wieder einkommentieren
+    labels = [int(i) for i in latest_x_days['DeltaConfirmed']]
+    rects = ax.patches
+
+    for rect, label in zip(rects, labels):
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width() / 2, height + 5, label,
+            ha='center', va='bottom', color='khaki', fontstyle='oblique', fontweight='bold')
+
+    labels_pct = ["%.2f \nPct" % i for i in latest_x_days['DeltaPercentage']]
+    for rect, label_pct in zip(rects, labels_pct):
+        height = rect.get_height()
+        if label_pct.startswith('-'):
+            label_pct = '-\n' + label_pct[1:]
+        else:
+            label_pct = '+\n' + label_pct[0:]
+            print (label_pct)
+
+        ax.text(rect.get_x() + rect.get_width() / 1.8, height - 400, label_pct,
+            ha='center', va='center', color='black', fontstyle='italic', fontsize = 8)
+    '''
+    
+    ax2 = ax.twinx()
+    #linechart = ax.plot(latest_x_days['datum'], latest_x_days['mean_last'+str(rolling_window_avg)+'days'], color='mediumvioletred', label='20 day avarage')
+    #linechart = ax.plot(latest_x_days_by['datum'], latest_x_days_by['mean_last'+str(rolling_window_avg)+'days'], color='mediumvioletred', label='20 day avarage')
+    ax2.set_yticklabels([])
+
+    loc = plticker.MultipleLocator(base=3.0) # this locator puts ticks at regular intervals
+    ax.xaxis.set_major_locator(loc)
+
+    #ax.grid(True, linestyle='-.')
+    ax.legend(loc="upper right")
+    #plt.tight_layout()
+    #f.savefig('plot.png', dpi=f.dpi)
+    f.savefig('plot.png')
+
+    ax.tick_params(axis="x", labelsize=8, colors='black')
+    ax.tick_params(axis="y", labelsize=8, colors='black')
+
+    plt.show()
+
+def upload_plot():
+    gs_folder = "darkshadow-share"
+    cmd = [f"gsutil cp plot.png gs://{gs_folder}/",
+           f"gsutil acl ch -u AllUsers:R gs://{gs_folder}/plot.png"
+          ]
+    for commands in cmd:
+        os.system(commands)
+
+
+def main():
+    data, data_by = retrieve_covid_data()
+    plot_data(data, data_by)
+    upload_plot()
+
+main()
